4ed7c0568c8cf17a5bd9985f052053fb6509319e
Daniel Bowring <daniel.t.bowring@student.uts.edu.au>
2013-03-01 01:27:35 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# WaniKani Wallpaper Generator for Javascript
+(c) 2012 Daniel Bowring
+
+Inspired by [WaniKani Wallpaper by akranis](https://github.com/akranis/wanikaniwallpaper)
+
+## Usage
+
+- Open the `wallpaper.html` file.
+- Change the settings to your liking.
+- Add your WaniKani [Public API key](http://www.wanikani.com/account)
+- Click "Query Kanji"
+- Click "Save"
+
+## Notes
+- Only tested in Chrome 25
+- Export SVG format - you will have to convert it to another format for most
+uses (PNG is a good choice here)
---
+++ wallpaper.js
@@ -0,0 +1,291 @@
+function heisig_order() {
+    return "一二三四五六七八九十口日月田目古吾冒朋明唱晶品呂昌早旭世胃旦胆亘凹凸旧自白百中千舌升昇丸寸専博占上下卓朝只貝貞員見児元頁頑凡負万句肌旬勺的首乙乱直具真工左右有賄貢項刀刃切召昭則副別丁町可頂子孔了女好如母貫兄克小少大多夕汐外名石肖硝砕砂削光太器臭妙省厚奇川州順水氷永泉原願泳沼沖江汁潮源活消況河泊湖測土吐圧埼垣圭封涯寺時均火炎煩淡灯畑災灰点照魚漁里黒墨鯉量厘埋同洞胴向尚字守完宣宵安宴寄富貯木林森桂柏枠梢棚杏桐植枯朴村相机本札暦案燥未末沫味妹朱株若草苦寛薄葉模漠墓暮膜苗兆桃眺犬状黙然荻狩猫牛特告先洗介界茶合塔王玉宝珠現狂皇呈全栓理主注柱金銑鉢銅釣針銘鎮道導辻迅造迫逃辺巡車連軌輸前各格略客額夏処条落冗軍輝運冠夢坑高享塾熟亭京涼景鯨舎周週士吉壮荘売学覚栄書津牧攻敗枚故敬言警計獄訂討訓詔詰話詠詩語読調談諾諭式試弐域賊栽載茂成城誠威滅減桟銭浅止歩渉頻肯企歴武賦正証政定錠走超赴越是題堤建延誕礎婿衣裁装裏壊哀遠猿初布帆幅帽幕幌錦市姉肺帯滞刺制製転芸雨雲曇雷霜冬天橋嬌立泣章競帝童瞳鐘商嫡適滴敵匕北背比昆皆混渇謁褐喝旨脂壱毎敏梅海乞乾腹複欠吹炊歌軟次茨資姿諮賠培剖音暗韻識鏡境亡盲妄荒望方妨坊芳肪訪放激脱説鋭曽増贈東棟凍妊廷染燃賓歳県栃地池虫蛍蛇虹蝶独蚕風己起妃改記包胞砲泡亀電竜滝豚逐遂家嫁豪腸場湯羊美洋詳鮮達羨差着唯焦礁集准進雑雌準奮奪確午許歓権観羽習翌曜濯曰困固国団因姻園回壇店庫庭庁床麻磨心忘忍認忌志誌忠串患思恩応意想息憩恵恐惑感憂寡忙悦恒悼悟怖慌悔憎慣愉惰慎憾憶慕添必泌手看摩我義議犠抹抱搭抄抗批招拓拍打拘捨拐摘挑指持括揮推揚提損拾担拠描操接掲掛研戒械鼻刑型才財材存在乃携及吸扱丈史吏更硬又双桑隻護獲奴怒友抜投没設撃殻支技枝肢茎怪軽叔督寂淑反坂板返販爪妥乳浮将奨採菜受授愛払広拡鉱弁雄台怠治始胎窓去法会至室到致互棄育撤充銃硫流允唆出山拙岩炭岐峠崩密蜜嵐崎入込分貧頒公松翁訟谷浴容溶欲裕鉛沿賞党堂常裳掌皮波婆披破被残殉殊殖列裂烈死葬瞬耳取趣最撮恥職聖敢聴懐慢漫買置罰寧濁環還夫扶渓規替賛潜失鉄迭臣姫蔵臓賢堅臨覧巨拒力男労募劣功勧努励加賀架脇脅協行律復得従徒待往征径彼役徳徹徴懲微街衡稿稼程税稚和移秒秋愁私秩秘称利梨穫穂稲香季委秀透誘穀菌米粉粘粒粧迷粋糧菊奥数楼類漆様求球救竹笑笠笹筋箱筆筒等算答策簿築人佐但住位仲体悠件仕他伏伝仏休仮伯俗信佳依例個健側侍停値倣倒偵僧億儀償仙催仁侮使便倍優伐宿傷保褒傑付符府任賃代袋貸化花貨傾何荷俊傍久畝囚内丙柄肉腐座卒傘匁以似併瓦瓶宮営善年夜液塚幣弊喚換融施旋遊旅勿物易賜尿尼泥塀履屋握屈掘堀居据層局遅漏刷尺尽沢訳択昼戸肩房扇炉戻涙雇顧啓示礼祥祝福祉社視奈尉慰款禁襟宗崇祭察擦由抽油袖宙届笛軸甲押岬挿申伸神捜果菓課裸斤析所祈近折哲逝誓暫漸断質斥訴昨詐作雪録尋急穏侵浸寝婦掃当争浄事唐糖康逮伊君群耐需儒端両満画歯曲曹遭漕槽斗料科図用庸備昔錯借惜措散廿庶遮席度渡奔噴墳憤焼暁半伴畔判券巻圏勝藤謄片版之乏芝不否杯矢矯族知智矛柔務霧班帰弓引弔弘強弱沸費第弟巧号朽誇汚与写身射謝老考孝教拷者煮著署暑諸猪渚賭峡狭挟追師帥官棺管父交効較校足促距路露跳躍践踏骨滑髄禍渦過阪阿際障随陪陽陳防附院陣隊墜降階陛隣隔隠堕陥穴空控突究窒窃窪搾窯窮探深丘岳兵浜糸織繕縮繁縦線締維羅練緒続絵統絞給絡結終級紀紅納紡紛紹経紳約細累索総綿絹繰継緑縁網緊紫縛縄幼後幽幾機玄畜蓄弦擁滋慈磁系係孫懸却脚卸御服命令零齢冷領鈴勇通踊疑擬凝範犯厄危宛腕苑怨柳卵留貿印興酉酒酌酵酷酬酪酢酔配酸猶尊豆頭短豊鼓喜樹皿血盆盟盗温監濫鑑猛盛塩銀恨根即爵節退限眼良朗浪娘食飯飲飢餓飾館養飽既概慨平呼坪評刈希凶胸離殺純鈍辛辞梓宰壁避新薪親幸執報叫糾収卑碑陸睦勢熱菱陵亥核刻該劾述術寒醸譲壌嬢毒素麦青精請情晴清静責績積債漬表俵潔契喫害轄割憲生星姓性牲産隆峰縫拝寿鋳籍春椿泰奏実奉俸棒謹勤漢嘆難華垂睡錘乗剰今含吟念琴陰予序預野兼嫌鎌謙廉西価要腰票漂標栗遷覆煙南楠献門問閲閥間簡開閉閣閑聞潤欄闘倉創非俳排悲罪輩扉侯候決快偉違緯衛韓干肝刊汗軒岸幹芋宇余除徐叙途斜塗束頼瀬勅疎速整剣険検倹重動勲働種衝薫病痴痘症疾痢疲疫痛癖匿匠医匹区枢殴欧抑仰迎登澄発廃僚寮療彫形影杉彩彰彦顔須膨参惨修珍診文対紋蚊斉剤済斎粛塁楽薬率渋摂央英映赤赦変跡蛮恋湾黄横把色絶艶肥甘紺某謀媒欺棋旗期碁基甚勘堪貴遺遣舞無組粗租祖阻査助宜畳並普譜湿顕繊霊業撲僕共供異翼洪港暴爆恭選殿井囲耕亜悪円角触解再講購構溝論倫輪偏遍編冊典氏紙婚低抵底民眠捕浦蒲舗補邸郭郡郊部都郵邦郷響郎廊盾循派脈衆逓段鍛后幻司伺詞飼嗣舟舶航般盤搬船艦艇瓜弧孤繭益暇敷来気汽飛沈妻衰衷面革靴覇声呉娯誤蒸承函極牙芽邪雅釈番審翻藩毛耗尾宅託為偽長張帳脹髪展喪巣単戦禅弾桜獣脳悩厳鎖挙誉猟鳥鳴鶴烏蔦鳩鶏島暖媛援緩属嘱偶遇愚隅逆塑岡鋼綱剛缶陶揺謡就懇墾免逸晩勉象像馬駒験騎駐駆駅騒駄驚篤騰虎虜膚虚戯虞慮劇虐鹿薦慶麗熊能態寅演辰辱震振娠唇農濃送関咲鬼醜魂魔魅塊襲嚇朕雰箇錬遵罷屯且藻隷癒丹潟丑卯巳々";
+}
+
+var wallpaper = (function () {
+var NAMESPACE = "http://www.w3.org/2000/svg";
+var svg = document.createElementNS(NAMESPACE, 'svg');
+svg.setAttributeNS(null, 'version', "1.1");
+var width, height, font_size, x_padding, y_padding;
+var x_margin, y_margin;
+var font_ratio = 19 / 16.0;
+var background_color = 'rgb(0, 0, 0)';
+var api_base_url = 'http://www.wanikani.com/api/';
+var API_KEY = '';
+
+
+var background = document.createElementNS(NAMESPACE, 'rect');
+background.setAttributeNS(null, 'width', '100%');
+background.setAttributeNS(null, 'height', '100%');
+background.setAttributeNS(null, 'fill', background_color);
+svg.appendChild(background);
+
+var levels = {
+    "error": "rgb(255, 0, 0)",
+    "unseen": "rgb(40, 40, 40)",
+    "apprentice": "rgb(221, 0, 147)",
+    "guru": "rgb(136, 45, 158)",
+    "master": "rgb(41, 77, 219)",
+    "enlightened": "rgb(0, 147, 221)",
+    "burned": "rgb(240, 240, 240)"
+};
+
+var Char = function(index, character) {
+    this.index = index;
+    this.character = character;
+    this.element = document.createElementNS(NAMESPACE, 'text');
+    this.element.setAttribute('class', 'character');
+    this.element.appendChild(document.createTextNode(character));
+    this.setLevel('unseen');
+};
+Char.prototype.setLevel = function(level) {
+    this.level = level;
+    this.element.setAttributeNS(null, 'fill', levels[level] || levels.error);
+};
+var CharCollection = function(characters) {
+    this.array = [];
+    if (arguments.length == 1) {
+        this.consume(characters);
+    }
+};
+CharCollection.prototype.consume = function(characters) {
+    for (var i=0; i<characters.length; ++i) {
+        this.add(characters[i]);
+    }
+};
+CharCollection.prototype.cautious_consume = function(characters) {
+    for (var i=0; i<characters.length; ++i) {
+        if (this.indexOf(characters[i]) < 0){
+            this.add(characters[i]);
+        }
+    }
+};
+CharCollection.prototype.add = function(character) {
+    var c = new Char(this.array.length, character);
+    this.array.push(c);
+    svg.appendChild(c.element);
+    return c;
+};
+CharCollection.prototype.getByIndex = function(index) {
+    return this.array[index];
+};
+CharCollection.prototype.getByCharacter = function(character) {
+    return this.array[this.indexOf(character)];
+};
+CharCollection.prototype.indexOf = function(character) {
+    for (var i=0; i<this.array.length; ++i) {
+        if (this.array[i].character == character) {
+            return i;
+        }
+    }
+    return -1;
+};
+CharCollection.prototype.each = function(fn) {
+    for (var i=0; i<this.array.length; ++i) {
+        fn(i, this.array[i], this.array);
+    }
+};
+
+var redraw_hold = 0;
+var redraw = function() {
+    if (redraw_hold > 0) {
+        return
+    };
+    svg.setAttributeNS(null, 'width', width);
+    svg.setAttributeNS(null, 'height', height);
+    var children = svg.getElementsByClassName('character');
+    var font_height = font_size * font_ratio;
+    var cols = Math.floor((width - 2 * x_margin - x_padding) / (font_size + x_padding));
+    var rows = Math.floor((height - 2 * y_margin - y_padding) / (font_height + y_padding));
+
+    svg.setAttributeNS(null, 'font-size', font_size);
+    characters.each(function(index, character, characters) {
+        var row = index % cols;
+        var col = Math.floor(index / cols);
+        character.element.setAttributeNS(null, 'x', x_margin + row * font_size + (row-1) * x_padding + x_margin);
+        character.element.setAttributeNS(null, 'y', y_margin + col * font_height + (col-1) * y_padding + font_height + y_margin);
+    });
+};
+
+var hold_redraw = function(fn, context) {
+    ++redraw_hold;
+    try {
+        fn.call(context);
+    } finally {
+        --redraw_hold;
+    }
+};
+
+var changeSize = function(w, h, f, px, py, mx, my) {
+    width = w;
+    height = h;
+    font_size = f;
+    x_padding = px ;
+    y_padding = py;
+    x_margin = mx;
+    y_margin = my;
+    redraw();
+};
+
+
+var JSONP = {};
+var JSONP_INDEX = 0;
+var globalize_function = function(fn, context) {
+    var name = 'response_' + JSONP_INDEX;
+    JSONP_INDEX += 1;
+    var wrapper = function() {
+        fn.apply(context, arguments);
+        delete JSONP[name];
+    }
+    JSONP[name] = wrapper;
+    return 'wallpaper.JSONP.' + name;
+};
+
+var send_request = function(url, success, error) {
+    var e = document.createElement('script');
+    if (error) {
+        e.onerror = error;
+    }
+    e.type = 'text/javascript';
+    e.charset = 'utf-8';
+    if (success) {
+        url += '?callback=' + globalize_function(success);
+    }
+    e.src = url;
+    document.getElementsByTagName('body')[0].appendChild(e);
+};
+
+var setWidth = function(pixels) {
+    width = pixels;
+    redraw();
+};
+var setHeight = function(pixels) {
+    height = pixels;
+    redraw();
+};
+var setFontSize = function(pixels) {
+    font_size = pixels;
+    redraw();
+};
+var setXPadding = function(pixels) {
+    x_padding = pixels;
+    redraw();
+};
+var setYPadding = function(pixels) {
+    y_padding = pixels;
+    redraw();
+};
+var setXMargin = function(pixels) {
+    x_margin = pixels;
+    redraw();
+};
+var setYMargin = function(pixels) {
+    y_margin = pixels;
+    redraw();
+};
+var setAPIKey = function(key) {
+    API_KEY = key;
+};
+
+
+var queryKanji = function() {
+    if (API_KEY.length != 32) {
+        alert('API KEY appears invalid (must be 32 characters long)');
+        return;
+    }
+    var url = api_base_url + 'user/' + API_KEY + '/kanji';
+    var success = function(response) {
+        var card, character, info = response.requested_information;
+        for (var i=0; i<info.length; ++i) {
+            card = info[i];
+            character = characters.getByCharacter(card.character);
+            if (!character) {
+                character = characters.add(card.character);
+                svg.appendChild(character.element);
+            }
+            if (card.stats) {
+                character.setLevel(card.stats.srs);
+            }
+        }
+        redraw();
+    };
+    var failure = function() {
+        console.log('failure');
+        alert('Failed to query kanji - check your api key');
+    };
+    send_request(url, success, failure);
+};
+
+var reset = function() {
+    // TODO: Empty SVG, empty characters
+    // 
+};
+
+var emptyCharacters = function() {
+    characters.each(function(i, e) {
+        svg.removeChild(e.element);
+    });
+    characters = new CharCollection();
+    redraw();
+};
+var AddHeisigOrder = function() {
+    characters.cautious_consume(heisig_order());
+    redraw();
+};
+
+var saveImage = function() {
+    var s = '<svg version="1.1" xmlns="' + NAMESPACE + '" font-size="';
+    s += font_size + '" width="' + width + '" height="' + height + '">';
+    s += '<rect width="100%" height="100%" background="';
+    s += background_color + '"></rect>';
+    characters.each(function(i, e) {
+        s += '<text x="';
+        s += e.element.getAttributeNS(null, 'x') || '0';
+        s += '" y="';
+        s += e.element.getAttributeNS(null, 'y') || '0';
+        s += '" fill="';
+        s += e.element.getAttributeNS(null, 'fill') || '';
+        s += '">' + e.character + '</text>';
+    });
+    s += '</svg>';
+    // console.log(s);
+    open('data:image/svg+xml;charset=utf-8,' + s);
+};
+
+var characters = new CharCollection(heisig_order());
+
+return {
+    changeSize: changeSize,
+    setWidth: setWidth,
+    setHeight: setHeight,
+    setFontSize: setFontSize,
+    setXPadding: setXPadding,
+    setYPadding: setYPadding,
+    setXMargin: setXMargin,
+    setYMargin: setYMargin,
+    queryKanji: queryKanji,
+    setAPIKey: setAPIKey,
+    redraw: redraw,
+    hold_redraw: hold_redraw,
+    emptyCharacters: emptyCharacters,
+    AddHeisigOrder: AddHeisigOrder,
+    saveImage: saveImage,
+    JSONP: JSONP,
+    getElement: function() {
+        return svg;
+    }
+};
+
+})();
+
+wallpaper.hold_redraw(function() {
+var inputs = document.getElementsByTagName('input');
+for (var i=0; i<inputs.length; ++i) {
+    if (inputs[i].onchange) {
+        inputs[i].onchange();
+    }
+}
+});
+wallpaper.redraw();
+// wallpaper.changeSize(1440, 1440, 16, 5, 5, 0, 0);
+document.getElementsByTagName('body')[0].appendChild(wallpaper.getElement());
